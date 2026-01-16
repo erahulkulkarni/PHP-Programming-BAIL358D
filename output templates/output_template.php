c7bb98918ad2b67d772e755ee6d4079fbce33bfe
erahulkulkarni <erahulkulkarni@gmail.com>
2026-01-16 05:43:30 +0530

    Add initial HTML template for output display

---
+++ output templates/output_template.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+  <head>
+    <title> Experiment name in short </title>
+  </head>
+  <body>
+    <p style="text-align: right;"> USN: YourUSN </p> 
+    <!-- This is HTML comment --> 
+    
+    <p style="text-align: right;"> Date: Day Month Year </p> 
+    <!-- paragraph (<p>) element --> 
+        
+    <p style="text-align: center;"> PHP Programming BAIL358D </p> 
+    <!-- Inline CSS to align the text to the center--> 
+  	
+    <p> Experiment Number. Experiment(also known as problem statement) </p> 
+    
+    <hr/> <!-- thematic break or horizontal line --> 
+    
+    <p style="text-align: center;"> Output: </p>    
+    
+    <!-- start of PHP code --> 
+  	<?php 
+  		  // PHP code goes here
+  	?> 
+  	<!-- end of PHP code --> 
+	
+  </body>
+</html>
